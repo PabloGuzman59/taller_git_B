2f713bc0917570dd0bd4e7dc377831cdff7a81f1
PabloGuzman59 <ghp1010717@est.univalle.edu>
2023-05-10 14:21:16 -0400

    XD

--- Program.cs
+++ Program.cs
@@ -8,8 +8,12 @@
             int n1= 5;
             int n2 = 5;
             int suma;
+            int edad;
+            Console.WriteLine("Ingrese su edad");
+            edad = int.Parse(Console.ReadLine());
             suma = n1 + n2;
             Console.WriteLine("Suma de dos numeros es " + suma);
+            Console.WriteLine(edad);
         }
     }
 }
