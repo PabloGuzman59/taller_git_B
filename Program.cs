7aee7d2c7e0e5087b6b76d20fca36580163c530c
PabloGuzman59 <ghp1010717@est.univalle.edu>
2023-05-10 14:12:11 -0400

    Mi resolusion

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Hello, World!");
+            int n1= 5;
+            int n2 = 5;
+            int suma;
+            suma = n1 + n2;
+            Console.WriteLine("Suma de dos numeros es " + suma);
+        }
+    }
+}
